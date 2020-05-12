111466a9836b1cefa2934213b3fb0210f3ccbba3
Paul van Gent <palkab29@gmail.com>
2020-05-12 16:42:51 +0200

    update demo

--- demo_COVID.py
+++ demo_COVID.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 from infection import infect, recover_or_die, compute_mortality
-from motion import update_positions, out_of_bounds, update_randoms,\
-set_destination, check_at_destination, keep_at_destination
+from motion import update_positions, out_of_bounds, update_randoms
+from path_planning import set_destination, check_at_destination, keep_at_destination
 from population import initialize_population, initialize_destination_matrix
 
 
