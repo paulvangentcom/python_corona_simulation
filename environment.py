ca00dcd55f0d4b4234a2019077821430dc776332
Paul van Gent <palkab29@gmail.com>
2020-05-26 14:47:25 +0200

    add method to load province polygon data

--- environment.py
+++ environment.py
@@ -50,3 +50,66 @@ def build_hospital(xmin, xmax, ymin, ymax, plt, addcross=True):
         plt.plot([xmiddle - (height / 2), xmiddle + (height / 2)],
                  [ymax + (height / 2), ymax + (height / 2)], color='red',
                  linewidth = 3)
+        
+        
+def load_province_data(names=['noordholland']):
+    '''returns the polygons defining individual provinces
+    
+    Function that returns polygons for each province in a dict object
+    
+    Keyword arguments
+    -----------------
+    names : list
+        list containing province names to load, without spaces.
+    
+    Returns
+    -------
+    provinces : dict
+        keyed dictionary object, containing provinces
+        
+    '''
+    provinces = {}
+    
+    for name in names:
+        provinces[name] = np.genfromtxt('source_data/%s.csv' %name, delimiter = ',', 
+                                        skip_header = 1)
+        
+    return provinces
+
+
+def build_country(provinces, province_origins={}):
+    '''takes province coordinates and builds country
+    
+    All province coordinates are normalised 0-1, this function
+    takes all the coordinate arrays and correctly constructs the Netherlands
+    from them. Returns all provinces in separate arrays 
+    
+    Keyword arguments
+    -----------------
+    provinces : dict
+        dict object containing provinces, as returned by load_province_data function
+        
+    province_origins : dict
+        keyed dict of centers for each province, determines where to place them.
+        keys need to be the same as for provinces
+    
+    Returns
+    -------
+    country : dict
+        dict object containing all provinces with revised coordinates
+    '''
+    
+    country = {}
+    province_origins['noordholland'] = [0, 0]
+    
+    #load province data
+    for province in provinces:
+        prov = np.copy(provinces[province])
+        
+        prov[:,0] += province_origins[province][0]
+        prov[:,1] += province_origins[province][1]
+        #place province based on center
+        
+        country[province] = np.copy(prov)
+    
+    return country
